8ee29a2f62433ebd44ff630a867c6064f522a674
Amen <362961910@qq.com>
2016-08-21 23:57:57 +0800

    添加模块之间以来注入，添加mongo头

--- Web/src/main/java/controller/HelloWorld.java
+++ Web/src/main/java/controller/HelloWorld.java
@@ -1,10 +1,15 @@
 package controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import service.test.StudentTestService;
 
 @RestController
 public class HelloWorld {
+
+    @Autowired
+    private StudentTestService studentTestService;
 
     @RequestMapping(value = "/home")
     public String home(){
